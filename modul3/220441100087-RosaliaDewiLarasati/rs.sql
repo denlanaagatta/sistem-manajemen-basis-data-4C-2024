ceb246362411453ac0f40be64a8c58e2210ef7b1
rosaliadwlaras <rosaliadewilarasati@gmail.com>
2024-04-25 17:53:23 +0700

    220441100087-RosaliaDewiLarasati-Modul3-KakFanky

---
+++ modul3/220441100087-RosaliaDewiLarasati/rs.sql
@@ -0,0 +1,115 @@
+-- IN
+DELIMITER /
+CREATE PROCEDURE pasien(
+    IN id INT,
+    IN nama VARCHAR(25)
+)
+BEGIN
+    SELECT * FROM pasien
+    WHERE id = id_pasien;
+END //
+CALL pasien(2, 'Rani');
+DROP PROCEDURE IF EXISTS pasien;
+DELIMITER ;
+
+-- OUT
+DELIMITER //
+CREATE PROCEDURE Count_Dokter_By_Spesialisasi(
+    IN spesialisasi_filter VARCHAR(255),
+    OUT total_dokter INT
+)
+BEGIN
+    SELECT COUNT(*) INTO total_dokter FROM dokter 
+    WHERE spesialisasi = spesialisasi_filter;
+END //
+CALL Count_Dokter_By_Spesialisasi('Dokter Umum', @total_dokter);
+SELECT @total_dokter AS total_dokter;
+DELIMITER ;
+
+-- INOUT
+DELIMITER //
+CREATE PROCEDURE Update_Stok_Obat(
+    INOUT id INT,
+    IN qty INT
+)
+BEGIN
+    UPDATE Obat
+    SET stok = stok + qty
+    WHERE id = id_obat;
+END //
+
+SET @id_obat := 6;
+CALL Update_Stok_Obat(@id_obat, 5);
+SELECT * FROM obat WHERE id_obat=6
+DROP PROCEDURE IF EXISTS Update_Stok_Obat;
+DELIMITER ;
+
+-- Memasukkan data baru
+DELIMITER //
+CREATE PROCEDURE Insert_New_Pasien(
+    IN id_pasien INT,
+    IN nama VARCHAR(25),
+    IN tgl_lahir DATE,
+    IN alamat VARCHAR(255),
+    IN jenis_kelamin VARCHAR(10),
+    IN no_telepon VARCHAR(15)
+)
+BEGIN
+    INSERT INTO pasien (id_pasien, nama, tgl_lahir, alamat, jenis_kelamin, no_telepon)
+    VALUES (id_pasien, nama, tgl_lahir, alamat, jenis_kelamin, no_telepon);
+END //
+CALL Insert_New_Pasien(21, 'Tania', '2000-05-05', '573 Main Street', 'Perempuan', '111111111');
+SELECT * FROM pasien;
+DELIMITER ;
+DROP PROCEDURE IF EXISTS Insert_New_Pasien;
+
+
+DELIMITER //
+CREATE PROCEDURE Insert_New_Rawat_Inap(
+    IN id_rawat_inap INT,
+    IN id_pasien INT,
+    IN id_dokter INT,
+    IN id_ruangan INT,
+    IN id_obat INT,
+    IN tgl_masuk DATE,
+    IN tgl_keluar DATE,
+    IN diagnosis TEXT,
+    IN stts VARCHAR(25),
+)
+BEGIN
+    INSERT INTO rawat_inap (id_rawat_inap, id_pasien, id_dokter, id_ruangan, id_obat, tgl_masuk, tgl_keluar, diagnosis, stts)
+    VALUES (id_rawat_inap, id_pasien, id_dokter, id_ruangan, id_obat, tgl_masuk, tgl_keluar, diagnosis, stts);
+END //
+
+CALL Insert_New_Pasien(, 'Tania', '2000-05-05', '573 Main Street', 'Perempuan', '111111111');
+SELECT * FROM rawat_inap;
+DELIMITER ;
+
+-- Memperbaharui data
+DELIMITER //
+CREATE PROCEDURE Update_Dokter_Spesialisasi(
+    IN id INT,
+    IN new_spesialisasi VARCHAR(255)
+)
+BEGIN
+    UPDATE Dokter
+    SET spesialisasi = new_spesialisasi
+    WHERE id = id_dokter;
+END //
+CALL Update_Dokter_Spesialisasi(2, 'Dokter Bedah');
+SELECT * FROM dokter WHERE id_dokter=2;
+SELECT * FROM dokter;
+DELIMITER ;
+
+-- Menghapus data
+DELIMITER //
+CREATE PROCEDURE Delete_Rawat_Inap(
+    IN id INT
+)
+BEGIN
+    DELETE FROM rawat_inap
+    WHERE id = id_rawat_inap;
+END //
+CALL Delete_Rawat_Inap(20);
+SELECT * FROM rawat_inap;
+DELIMITER ;
